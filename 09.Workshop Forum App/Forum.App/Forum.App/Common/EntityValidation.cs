edef9d448b91136daca305e4117365a96eee7b0c
Deniz <d.ivan0v.6100@gmail.com>
2023-06-30 00:00:24 +0300

    commit

---
+++ 09.Workshop Forum App/Forum.App/Forum.App/Common/EntityValidation.cs
@@ -0,0 +1,13 @@
+﻿namespace Forum.App.Common
+{
+    public class EntityValidation
+    {
+        public static class Post
+        {
+            public const int TitleMinLentgth = 10;
+            public const int TitleMaxLentgth = 50;
+            public const int ContentMinLentgth = 30;
+            public const int ContentMaxLentgth = 1500;
+        }
+    }
+}
