cc7c9910c2e6d13403b6904304d720d2b35744e1
Deniz Ivanov <d.ivan0v.6100@gmail.com>
2023-10-10 22:33:42 +0300

    Added two new buttons

--- 12.Exam Preparation/Library/Library/Controllers/BookController.cs
+++ 12.Exam Preparation/Library/Library/Controllers/BookController.cs
@@ -25,5 +25,37 @@ namespace Library.Web.Controllers
 
             return View(model);
         }
+
+        public async Task<IActionResult> AddToCollection(int id)
+        {
+            var book = await bookService.GetBookIdAsync(id);
+
+            if (book == null)
+            {
+                return RedirectToAction(nameof(All));
+            }
+
+            var userId = GetUserId();
+
+            await bookService.AddBookToCollectionAsync(userId, book);
+
+            return RedirectToAction(nameof(Mine));
+        }
+
+        public async Task<IActionResult> RemoveFromCollection(int id)
+        {
+            var book = await bookService.GetBookIdAsync(id);
+
+            if (book == null)
+            {
+                return RedirectToAction(nameof(Mine));
+            }
+
+            var userId = GetUserId();
+
+            await bookService.RemoveBookToCollectionAsync(userId, book);
+
+            return RedirectToAction(nameof(Mine));
+        }
     }
 }
